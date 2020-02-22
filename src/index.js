e47ac6a774ad888cd74585075d2941153dca9edb
Hugo Courthias <hugo.courthias@gmail.com>
2020-02-22 13:08:05 +0100

    Added: expo to peer dependencies

--- src/index.js
+++ src/index.js
@@ -1,15 +1,71 @@
-import auth from './oauth/auth';
-import {get, post} from './rest';
-import stream from './stream';
+import { AuthSession } from "expo";
+import URLSearchParams from "@ungap/url-search-params";
 
-export {auth};
+import request from "./request";
 
-export default function twitter(tokens) {
-  return {
-    stream: stream.bind(undefined, tokens),
-    rest: {
-      get: get.bind(undefined, tokens),
-      post: post.bind(undefined, tokens),
+function getRequestToken(tokens, callbackUrl, accessType) {
+  const method = "POST";
+  const url = "https://api.twitter.com/oauth/request_token";
+  const body = accessType ? { x_auth_access_type: accessType } : {};
+
+  return request(tokens, url, { method, body }, { oauth_callback: callbackUrl })
+    .then(response => response.text())
+    .then(text => {
+      const params = new URLSearchParams(text);
+      return {
+        requestToken: params.get("oauth_token"),
+        requestTokenSecret: params.get("oauth_token_secret")
+      };
+    });
+}
+
+function getAccessToken(
+  { consumerKey, consumerSecret, requestToken, requestTokenSecret },
+  oauthVerifier
+) {
+  const method = "POST";
+  const url = "https://api.twitter.com/oauth/access_token";
+  return request(
+    {
+      consumerKey,
+      consumerSecret,
+      oauthToken: requestToken,
+      oauthTokenSecret: requestTokenSecret
     },
-  };
+    url,
+    { method },
+    { oauth_verifier: oauthVerifier }
+  )
+    .then(response => response.text())
+    .then(text => {
+      const params = new URLSearchParams(text);
+      return {
+        accessToken: params.get("oauth_token"),
+        accessTokenSecret: params.get("oauth_token_secret"),
+        id: params.get("user_id"),
+        name: params.get("screen_name")
+      };
+    });
+}
+
+export default async function auth(tokens, callbackUrl, { accessType } = {}) {
+  const { requestToken, requestTokenSecret } = await getRequestToken(
+    tokens,
+    callbackUrl,
+    accessType
+  );
+  AuthSession.startAsync({
+    authUrl: `https://api.twitter.com/oauth/authorize?oauth_token=${requestToken}`
+  }).then(async result => {
+    const a = await getAccessToken(
+      {
+        consumerKey: tokens.consumerKey,
+        consumerSecret: tokens.consumerSecret,
+        requestToken,
+        requestTokenSecret
+      },
+      result.params.oauth_verifier
+    );
+    console.log(a);
+  });
 }
